15cd39ba1a2ef7a5701bd8f92de033140e0b042c
Li Lei <13186102535@163.com>
2021-03-10 08:54:01 +0800

    H_StringToIntegerAtoi

--- src/main/java/com/futurell/leetcode/H_StringToIntegerAtoi.java
+++ src/main/java/com/futurell/leetcode/H_StringToIntegerAtoi.java
@@ -1,5 +1,8 @@
 package com.futurell.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Description: 8. 字符串转换整数 (atoi)
  * @Author: lilei58
@@ -67,12 +70,53 @@ package com.futurell.leetcode;
 public class H_StringToIntegerAtoi {
 
     public static void main(String[] args) {
-        int index = myAtoi("4193 with words");
+        int index = myAtoi("00000-42a1234");
         int i = 0;
     }
 
-    public static int myAtoi(String s) {
+    /**
+     * 思路: 状态机
+     */
+    public static int myAtoi(String str) {
+        Automaton automaton = new Automaton();
+        for (int i = 0; i < str.length(); i++) {
+            automaton.get(str.charAt(i));
+        }
+        return (int) (automaton.sign * automaton.ans);
+    }
+}
 
-        return 0;
+class Automaton {
+    public int sign = 1;
+    public long ans = 0;
+    private String state = "start";
+    private Map<String, String[]> table = new HashMap<String, String[]>() {{
+        put("start", new String[]{"start", "signed", "in_number", "end"});
+        put("signed", new String[]{"end", "end", "in_number", "end"});
+        put("in_number", new String[]{"end", "end", "in_number", "end"});
+        put("end", new String[]{"end", "end", "end", "end"});
+    }};
+
+    public void get(char c) {
+        state = table.get(state)[get_col(c)];
+        if ("in_number".equals(state)) {
+            ans = ans * 10 + c - '0';
+            ans = sign == 1 ? Math.min(ans, (long) Integer.MAX_VALUE) : Math.min(ans, -(long) Integer.MIN_VALUE);
+        } else if ("signed".equals(state)) {
+            sign = c == '+' ? 1 : -1;
+        }
+    }
+
+    private int get_col(char c) {
+        if (c == ' ') {
+            return 0;
+        }
+        if (c == '+' || c == '-') {
+            return 1;
+        }
+        if (Character.isDigit(c)) {
+            return 2;
+        }
+        return 3;
     }
 }
