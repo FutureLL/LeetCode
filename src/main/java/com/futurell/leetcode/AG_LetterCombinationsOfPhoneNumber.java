0884bb6056f95c15c3801ae299a5e007f9239e7f
lilei <13186102535@163.com>
2021-04-29 08:56:32 +0800

    AH_4Sum

---
+++ src/main/java/com/futurell/leetcode/AG_LetterCombinationsOfPhoneNumber.java
@@ -0,0 +1,84 @@
+package com.futurell.leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Description: 17. 电话号码的字母组合
+ * @Author: lilei58
+ * @Date: Created in 2021/4/27 7:59 下午
+ *
+ * 示例 1：
+ *  输入：digits = "23"
+ *  输出：["ad","ae","af","bd","be","bf","cd","ce","cf"]
+ *
+ * 示例 2：
+ *  输入：digits = ""
+ *  输出：[]
+ *
+ * 示例 3：
+ *  输入：digits = "2"
+ *  输出：["a","b","c"]
+ */
+public class AG_LetterCombinationsOfPhoneNumber {
+
+    public static void main(String[] args) {
+        List<String> list = letterCombinations("23");
+        int i = 0;
+    }
+
+    /**
+     *  思路: 回溯法
+     * 1. 每次都循环
+     */
+    public static List<String> letterCombinations(String digits) {
+        // 返回数据
+        List<String> combinations = new ArrayList<>();
+        if (digits.length() == 0) {
+            return combinations;
+        }
+
+        // 数据对应
+        Map<Character, String> phoneMap = new HashMap<Character, String>() {{
+            put('2', "abc");
+            put('3', "def");
+            put('4', "ghi");
+            put('5', "jkl");
+            put('6', "mno");
+            put('7', "pqrs");
+            put('8', "tuv");
+            put('9', "wxyz");
+        }};
+
+        // 回溯方法
+        backtrack(combinations, phoneMap, digits, 0, new StringBuffer());
+        return combinations;
+    }
+
+    /**
+     * 回溯方法
+     * @param combinations 返回数据
+     * @param phoneMap     号码对应字母
+     * @param digits       查询字符串
+     * @param index        下标开始
+     * @param sb           组装字符串
+     * @return
+     */
+    private static void backtrack(List<String> combinations, Map<Character, String> phoneMap, String digits, int index, StringBuffer sb) {
+        if (index == digits.length()) {
+            combinations.add(sb.toString());
+        } else {
+            // 分离单独字符
+            char ch = digits.charAt(index);
+            String letters = phoneMap.get(ch);
+            int lettersCount = letters.length();
+            for (int i = 0; i < lettersCount; i++) {
+                sb.append(letters.charAt(i));
+                backtrack(combinations, phoneMap, digits, index + 1, sb);
+                sb.deleteCharAt(index);
+            }
+        }
+    }
+}
---
+++ src/main/java/com/futurell/leetcode/AH_4Sum.java
@@ -0,0 +1,97 @@
+package com.futurell.leetcode;
+
+import com.sun.deploy.net.proxy.StaticProxyManager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @description: 18. 四数之和
+ * @author: Mr.Li
+ * @date: Created in 2021/4/28 8:23
+ * @version: 1.0
+ * @modified By:
+ *
+ * 给定一个包含 n 个整数的数组 nums 和一个目标值 target,判断 nums 中是否存在四个元素 a,b,c和d,使得 a+b+c+d 的值与 target 相等？
+ * 找出所有满足条件且不重复的四元组。
+ *
+ * 示例 1：
+ *  输入：nums = [1,0,-1,0,-2,2], target = 0
+ *  输出：[[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+ *
+ * 示例 2：
+ *  输入：nums = [], target = 0
+ *  输出：[]
+ *
+ *
+ */
+public class AH_4Sum {
+
+    public static void main(String[] args) {
+
+        List<List<Integer>> lists = fourSum(new int[]{1, 0, -1, 0, -2, 2}, 0);
+        int i = 0;
+    }
+
+    /**
+     *  思路: 双指针 + 两层for循环
+     * 1. 在前面做过的三数之和中增加一层for循环即可
+     */
+    public static List<List<Integer>> fourSum(int[] nums, int target) {
+
+        List<List<Integer>> resultReturn = new ArrayList<>();
+
+        // 增序排列
+        Arrays.sort(nums);
+
+        // 将第一个固定
+        for (int i = 0; i < nums.length - 3; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            if (nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) {
+                break;
+            }
+            if (nums[i] + nums[nums.length - 3] + nums[nums.length - 2] + nums[nums.length - 1] < target) {
+                continue;
+            }
+
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                if (nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) {
+                    break;
+                }
+                if (nums[i] + nums[j] + nums[nums.length - 2] + nums[nums.length - 1] < target) {
+                    continue;
+                }
+
+                // 定义两个指针
+                int L = j + 1, R = nums.length - 1;
+                while (L < R) {
+                    // 计算四数之和
+                    int sum = nums[i] + nums[j] + nums[L] + nums[R];
+                    if (sum == target) {
+
+                        List<Integer> list = new ArrayList<>();
+                        list.addAll(Arrays.asList(nums[i], nums[j], nums[L], nums[R]));
+                        if (!resultReturn.contains(list)) {
+                            resultReturn.add(list);
+                        }
+                        L ++;
+                        R --;
+                    } else if (sum < target) {
+                        L ++;
+                    } else {
+                        R --;
+                    }
+                }
+            }
+        }
+
+        return resultReturn;
+    }
+}
