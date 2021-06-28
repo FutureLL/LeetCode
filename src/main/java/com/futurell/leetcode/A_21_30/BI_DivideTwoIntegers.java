7f607b4a43efc50054dca9f1bc74512fdcc8ba7f
lilei58 <lilei58@guazi.com>
2021-06-28 19:35:13 +0800

    BH_ImplementStrstr

---
+++ src/main/java/com/futurell/leetcode/A_21_30/BI_DivideTwoIntegers.java
@@ -0,0 +1,33 @@
+package com.futurell.leetcode.A_21_30;
+
+/**
+ * @Description: 29. 两数相除
+ * @Author: lilei58
+ * @Date: Created in 2021/6/28 3:00 下午
+ *
+ * 给定两个整数，被除数 dividend 和除数 divisor。将两数相除，要求不使用乘法、除法和 mod 运算符。
+ * 返回被除数 dividend 除以除数 divisor 得到的商。
+ * 整数除法的结果应当截去（truncate）其小数部分，例如：truncate(8.345) = 8 以及 truncate(-2.7335) = -2
+ *
+ * 示例 1:
+ *  输入: dividend = 10, divisor = 3
+ *  输出: 3
+ *  解释: 10/3 = truncate(3.33333..) = truncate(3) = 3
+ *
+ * 示例 2:
+ *  输入: dividend = 7, divisor = -3
+ *  输出: -2
+ *  解释: 7/-3 = truncate(-2.33333..) = -2
+ */
+public class BI_DivideTwoIntegers {
+
+    public static void main(String[] args) {
+        int divide = divide(10, 3);
+        int i = 0;
+    }
+
+    public static int divide(int dividend, int divisor) {
+
+        return 0;
+    }
+}
