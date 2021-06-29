05aaf213d4f25ce4d01663da15dfd183fadc7ff1
lilei58 <lilei58@guazi.com>
2021-06-29 16:58:35 +0800

    BI_DivideTwoIntegers

--- src/main/java/com/futurell/leetcode/A_21_30/BI_DivideTwoIntegers.java
+++ src/main/java/com/futurell/leetcode/A_21_30/BI_DivideTwoIntegers.java
@@ -22,12 +22,44 @@ package com.futurell.leetcode.A_21_30;
 public class BI_DivideTwoIntegers {
 
     public static void main(String[] args) {
-        int divide = divide(10, 3);
+        int divide = divide(27, 3);
         int i = 0;
     }
 
     public static int divide(int dividend, int divisor) {
 
-        return 0;
+        // 特殊情况
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+        // 判断是否需要正负号
+        boolean isNegative = (dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0);
+        // int -> long
+        long dividendL = dividend;
+        long divisorL = divisor;
+        // 这里不能直接写成 long dividendL=Math.abs(dividend)
+        // 因为 Integer.MIN_VALUE 直接取绝对值会越界,先转成 long 再取绝对值
+        dividendL = Math.abs(dividendL);
+        divisorL = Math.abs(divisorL);
+
+        // 记录商
+        int value = 0;
+        while (divisorL <= dividendL) {
+            long temp = divisorL;
+            // 记录次数
+            long times = 1;
+            // tmp << 1 相当于 tmp*2,tmp*2 如果大于被除数了,就应该跳出去
+            // 比如 6*2=12 已经大于 10 了,就应该跳出内层的 while 循环了
+            // 使用内循环可以解决除数与被除数差距过大的情况
+            // 比如: 27/3, 3*2=6 -> 6*2=12 -> 12*2=24 -> 24*2不满足条件不进入 while 循环
+            // 很大程度上提升了效率
+            while (temp << 1 <= dividendL) {
+                temp = temp << 1;
+                times = times << 1;
+            }
+            value += times;
+            dividendL -= temp;
+        }
+        return isNegative ? -value : value;
     }
 }
