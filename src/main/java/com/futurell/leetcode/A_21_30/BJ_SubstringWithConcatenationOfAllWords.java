973030130b07896236283e56bed4130b58d0ba17
lilei58 <lilei58@guazi.com>
2021-06-29 19:25:19 +0800

    BJ_SubstringWithConcatenationOfAllWords

--- src/main/java/com/futurell/leetcode/A_21_30/BJ_SubstringWithConcatenationOfAllWords.java
+++ src/main/java/com/futurell/leetcode/A_21_30/BJ_SubstringWithConcatenationOfAllWords.java
@@ -68,11 +68,10 @@ public class BJ_SubstringWithConcatenationOfAllWords {
 
     public static List<Integer> findSubstring2(String s, String[] words) {
         List<Integer> res = new ArrayList<>();
-        int wordNum = words.length;
-        if (wordNum == 0) {
+        if (words.length == 0) {
             return res;
         }
-        int wordLen = words[0].length();
+
         // HashMap1 存所有单词
         HashMap<String, Integer> allWords = new HashMap<>();
         for (String w : words) {
@@ -80,13 +79,13 @@ public class BJ_SubstringWithConcatenationOfAllWords {
             allWords.put(w, value + 1);
         }
         // 遍历所有子串
-        for (int i = 0; i < s.length() - wordNum * wordLen + 1; i++) {
+        for (int i = 0; i < s.length() - words.length * words[0].length() + 1; i++) {
             // HashMap2 存当前扫描的字符串含有的单词
             HashMap<String, Integer> hasWords = new HashMap<>();
             int num = 0;
             // 判断该子串是否符合
-            while (num < wordNum) {
-                String word = s.substring(i + num * wordLen, i + (num + 1) * wordLen);
+            while (num < words.length) {
+                String word = s.substring(i + num * words[0].length(), i + (num + 1) * words[0].length());
                 // 判断该单词在 HashMap1 中
                 if (allWords.containsKey(word)) {
                     int value = hasWords.getOrDefault(word, 0);
@@ -101,7 +100,7 @@ public class BJ_SubstringWithConcatenationOfAllWords {
                 num++;
             }
             // 判断是不是所有的单词都符合条件
-            if (num == wordNum) {
+            if (num == words.length) {
                 res.add(i);
             }
         }
@@ -110,12 +109,13 @@ public class BJ_SubstringWithConcatenationOfAllWords {
 
     public static List<Integer> findSubstring3(String s, String[] words) {
         List<Integer> res = new ArrayList<>();
-        int wordNum = words.length;
-        if (wordNum == 0) {
+        if (words.length == 0) {
             return res;
         }
+
         int wordLen = words[0].length();
         HashMap<String, Integer> allWords = new HashMap<>();
+
         for (String w : words) {
             int value = allWords.getOrDefault(w, 0);
             allWords.put(w, value + 1);
@@ -126,17 +126,16 @@ public class BJ_SubstringWithConcatenationOfAllWords {
             // 记录当前 HashMap2（这里的 hasWords 变量）中有多少个单词
             int num = 0;
             // 每次移动一个单词长度
-            for (int i = j; i < s.length() - wordNum * wordLen + 1; i = i + wordLen) {
+            for (int i = j; i < s.length() - words.length * wordLen + 1; i = i + wordLen) {
                 // 防止情况三移除后,情况一继续移除
                 boolean hasRemoved = false;
-                while (num < wordNum) {
+                while (num < words.length) {
                     String word = s.substring(i + num * wordLen, i + (num + 1) * wordLen);
                     if (allWords.containsKey(word)) {
                         int value = hasWords.getOrDefault(word, 0);
                         hasWords.put(word, value + 1);
                         // 出现情况三,遇到了符合的单词,但是次数超了
                         if (hasWords.get(word) > allWords.get(word)) {
-                            // hasWords.put(word, value);
                             hasRemoved = true;
                             int removeNum = 0;
                             // 一直移除单词,直到次数符合了
@@ -163,7 +162,7 @@ public class BJ_SubstringWithConcatenationOfAllWords {
                     }
                     num++;
                 }
-                if (num == wordNum) {
+                if (num == words.length) {
                     res.add(i);
 
                 }
