43a757b882ad2435d5a342e365d00ec5e012739e
lilei58 <lilei58@guazi.com>
2021-06-29 20:44:43 +0800

    BJ_SubstringWithConcatenationOfAllWords

--- src/main/java/com/futurell/leetcode/A_21_30/BJ_SubstringWithConcatenationOfAllWords.java
+++ src/main/java/com/futurell/leetcode/A_21_30/BJ_SubstringWithConcatenationOfAllWords.java
@@ -30,7 +30,7 @@ public class BJ_SubstringWithConcatenationOfAllWords {
 
     public static void main(String[] args) {
 
-        List<Integer> list = findSubstring("barfoofoobarthefoobarman", new String[]{"bar", "foo", "the"});
+        List<Integer> list = findSubstring("barfoothefoobarman", new String[]{"foo","bar"});
         int i = 0;
     }
 
@@ -38,28 +38,34 @@ public class BJ_SubstringWithConcatenationOfAllWords {
      *  思路:
      * 1. 滑动窗口 + 哈希表 --- findSubstring
      * 2. 判断每个子串是否符合,每次移动一个下标位置 --- findSubstring2
-     * 3. 判断每个子串是否符合,每次移动一个字符串 --- findSubstring3
      */
     public static List<Integer> findSubstring(String s, String[] words) {
+        // 存所有单词
         Map<String, Integer> allWords = new HashMap<>();
         for (String word : words) {
             allWords.put(word, allWords.getOrDefault(word, 0) + 1);
         }
-        int wordNum = words.length;
-        int wordLen = words[0].length();
+
+        // 存储满足条件的下标
         List<Integer> res = new ArrayList<>();
-        for (int i = 0; i < s.length() - wordNum * wordLen + 1; i++) {
+        // 循环给定字符串
+        for (int i = 0; i < s.length() - words.length * words[0].length() + 1; i++) {
             Map<String, Integer> subWords = new HashMap<>();
             int index = i;
-            while (index < i + wordNum * wordLen) {
-                String curWord = s.substring(index, index + wordLen);
-                if (!allWords.containsKey(curWord) || subWords.get(curWord).equals(allWords.get(curWord))) {
+            // 相当于窗口,每次滑动一个单词的长度 index += words[0].length()
+            while (index < i + words.length * words[0].length()) {
+                // 截取单个单词
+                String curWord = s.substring(index, index + words[0].length());
+                // 两中情况结束循环:
+                // 1. 截取的字符串在allWords中不存在
+                // 2. 截取的字符串在allWords中存在 且 子串集合中截取字符串的量等于allWords中该截取字符串中的量
+                if (!allWords.containsKey(curWord) || subWords.get(curWord) == allWords.get(curWord)) {
                     break;
                 }
                 subWords.put(curWord, subWords.getOrDefault(curWord, 0) + 1);
-                index += wordLen;
+                index += words[0].length();
             }
-            if (index == i + wordNum * wordLen) {
+            if (index == i + words.length * words[0].length()) {
                 res.add(i);
             }
         }
@@ -75,9 +81,11 @@ public class BJ_SubstringWithConcatenationOfAllWords {
         // HashMap1 存所有单词
         HashMap<String, Integer> allWords = new HashMap<>();
         for (String w : words) {
+            // 获取对应 key 的 value,没有默认为 0
             int value = allWords.getOrDefault(w, 0);
             allWords.put(w, value + 1);
         }
+
         // 遍历所有子串
         for (int i = 0; i < s.length() - words.length * words[0].length() + 1; i++) {
             // HashMap2 存当前扫描的字符串含有的单词
@@ -85,6 +93,7 @@ public class BJ_SubstringWithConcatenationOfAllWords {
             int num = 0;
             // 判断该子串是否符合
             while (num < words.length) {
+                // 截取对应的字符串
                 String word = s.substring(i + num * words[0].length(), i + (num + 1) * words[0].length());
                 // 判断该单词在 HashMap1 中
                 if (allWords.containsKey(word)) {
@@ -103,79 +112,6 @@ public class BJ_SubstringWithConcatenationOfAllWords {
             if (num == words.length) {
                 res.add(i);
             }
-        }
-        return res;
-    }
-
-    public static List<Integer> findSubstring3(String s, String[] words) {
-        List<Integer> res = new ArrayList<>();
-        if (words.length == 0) {
-            return res;
-        }
-
-        int wordLen = words[0].length();
-        HashMap<String, Integer> allWords = new HashMap<>();
-
-        for (String w : words) {
-            int value = allWords.getOrDefault(w, 0);
-            allWords.put(w, value + 1);
-        }
-        // 将所有移动分成 wordLen 类情况
-        for (int j = 0; j < wordLen; j++) {
-            HashMap<String, Integer> hasWords = new HashMap<>();
-            // 记录当前 HashMap2（这里的 hasWords 变量）中有多少个单词
-            int num = 0;
-            // 每次移动一个单词长度
-            for (int i = j; i < s.length() - words.length * wordLen + 1; i = i + wordLen) {
-                // 防止情况三移除后,情况一继续移除
-                boolean hasRemoved = false;
-                while (num < words.length) {
-                    String word = s.substring(i + num * wordLen, i + (num + 1) * wordLen);
-                    if (allWords.containsKey(word)) {
-                        int value = hasWords.getOrDefault(word, 0);
-                        hasWords.put(word, value + 1);
-                        // 出现情况三,遇到了符合的单词,但是次数超了
-                        if (hasWords.get(word) > allWords.get(word)) {
-                            hasRemoved = true;
-                            int removeNum = 0;
-                            // 一直移除单词,直到次数符合了
-                            while (hasWords.get(word) > allWords.get(word)) {
-                                String firstWord = s.substring(i + removeNum * wordLen, i + (removeNum + 1) * wordLen);
-                                int v = hasWords.get(firstWord);
-                                hasWords.put(firstWord, v - 1);
-                                removeNum++;
-                            }
-                            // 加 1 是因为我们把当前单词加入到了 HashMap 2 中
-                            num = num - removeNum + 1;
-                            //这里依旧是考虑到了最外层的 for 循环,看情况二的解释
-                            i = i + (removeNum - 1) * wordLen;
-                            break;
-                        }
-                        // 出现情况二,遇到了不匹配的单词,直接将 i 移动到该单词的后边（但其实这里
-                        // 只是移动到了出现问题单词的地方,因为最外层有 for 循环,i 还会移动一个单词
-                        // 然后刚好就移动到了单词后边）
-                    } else {
-                        hasWords.clear();
-                        i = i + num * wordLen;
-                        num = 0;
-                        break;
-                    }
-                    num++;
-                }
-                if (num == words.length) {
-                    res.add(i);
-
-                }
-                // 出现情况一,子串完全匹配,我们将上一个子串的第一个单词从 HashMap2 中移除
-                if (num > 0 && !hasRemoved) {
-                    String firstWord = s.substring(i, i + wordLen);
-                    int v = hasWords.get(firstWord);
-                    hasWords.put(firstWord, v - 1);
-                    num = num - 1;
-                }
-
-            }
-
         }
         return res;
     }
