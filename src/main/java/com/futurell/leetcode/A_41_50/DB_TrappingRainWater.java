0725294f252fd0fc6e50341ace58125cc7c2a393
lilei58 <13186102535@163.com>
2021-07-29 21:29:21 +0800

    master DA_FirstMissingPositive

--- src/main/java/com/futurell/leetcode/A_41_50/DB_TrappingRainWater.java
+++ src/main/java/com/futurell/leetcode/A_41_50/DB_TrappingRainWater.java
@@ -19,7 +19,7 @@ public class DB_TrappingRainWater {
 
     public static void main(String[] args) {
 
-        int trap = trap2(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1});
+        int trap = trap3(new int[]{4, 2, 0, 3, 2, 5});
         int i = 0;
     }
 
