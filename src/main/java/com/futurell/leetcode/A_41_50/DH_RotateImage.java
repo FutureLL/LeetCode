2af36c51a625b30e13b9858b4649d727e580fcc7
lilei58 <13186102535@163.com>
2021-09-07 09:39:05 +0800

    DH_RotateImage

--- src/main/java/com/futurell/leetcode/A_41_50/DH_RotateImage.java
+++ src/main/java/com/futurell/leetcode/A_41_50/DH_RotateImage.java
@@ -1,1 +1,96 @@
-package com.futurell.leetcode.A_41_50;/** * @Description: 48. 旋转图像 * @Author: lilei58 * @Date: Created in 2021/9/7 上午7:21 * 给定一个 n × n 的二维矩阵 matrix 表示一个图像。请你将图像顺时针旋转 90 度。 * 你必须在"原地"旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要 使用另一个矩阵来旋转图像。 * * 示例 1： *  输入：matrix = [[1,2,3],[4,5,6],[7,8,9]] *  输出：[[7,4,1],[8,5,2],[9,6,3]] * * 示例 2： *  输入：matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]] *  输出：[[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]] * * 示例 3： *  输入：matrix = [[1]] *  输出：[[1]] * * 示例 4： *  输入：matrix = [[1,2],[3,4]] *  输出：[[3,1],[4,2]] */public class DH_RotateImage {    public static void main(String[] args) {        int[][] arr = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};        rotate(arr);        int i = 0;    }    /**     *  思路:     * 1.     * 2. 原地旋转 --- rotate2     */    public static void rotate(int[][] matrix) {        // 记录位置是否被覆盖        boolean[][] arr = new boolean[matrix.length][matrix.length];        for (int z = 0; z < matrix.length; z++) {            // 标记坐标            int i = 0, j = z;            int tempValueNew = matrix[i][j];            for (int whileCount = 0; whileCount < 4; whileCount++) {                // 记录旧值                int tempValueOld = tempValueNew;                // 计算要填入的坐标                switch (whileCount) {                    case 0:                    case 2:                        j = matrix.length - 1 - j;                        break;                    case 1:                    case 3:                        i = matrix.length - 1 - i;                        break;                    default:                        break;                }                if (!arr[i][j]) {                    // 记录被覆盖的值                    tempValueNew = matrix[i][j];                    arr[i][j] = true;                    // 填充值                    matrix[i][j] = tempValueOld;                }            }        }    }    public static void rotate2(int[][] matrix) {        int n = matrix.length;        for (int i = 0; i < n / 2; ++i) {            for (int j = 0; j < (n + 1) / 2; ++j) {                int temp = matrix[i][j];                matrix[i][j] = matrix[n - j - 1][i];                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];                matrix[j][n - i - 1] = temp;            }        }    }}
+package com.futurell.leetcode.A_41_50;
+
+/**
+ * @Description: 48. 旋转图像
+ * @Author: lilei58
+ * @Date: Created in 2021/9/7 上午7:21
+ * 给定一个 n × n 的二维矩阵 matrix 表示一个图像。请你将图像顺时针旋转 90 度。
+ * 你必须在"原地"旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要 使用另一个矩阵来旋转图像。
+ *
+ * 示例 1：
+ *  输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+ *  输出：[[7,4,1],[8,5,2],[9,6,3]]
+ *
+ * 示例 2：
+ *  输入：matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+ *  输出：[[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+ *
+ * 示例 3：
+ *  输入：matrix = [[1]]
+ *  输出：[[1]]
+ *
+ * 示例 4：
+ *  输入：matrix = [[1,2],[3,4]]
+ *  输出：[[3,1],[4,2]]
+ */
+public class DH_RotateImage {
+
+    public static void main(String[] args) {
+
+        int[][] arr = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+        rotate(arr);
+
+        int i = 0;
+    }
+
+    /**
+     *  思路:
+     * 1.
+     * 2. 原地旋转 --- rotate2
+     */
+    public static void rotate(int[][] matrix) {
+
+        // 记录位置是否被覆盖
+        boolean[][] arr = new boolean[matrix.length][matrix.length];
+
+        for (int z = 0; z < matrix.length; z++) {
+            // 标记坐标
+            int i = 0, j = z;
+            int tempValueNew = matrix[i][j];
+            for (int whileCount = 0; whileCount < 4; whileCount++) {
+
+                // 记录旧值
+                int tempValueOld = tempValueNew;
+
+                // 计算要填入的坐标
+                switch (whileCount) {
+                    case 0:
+                    case 2:
+                        //i = matrix.length - 1 - i;
+                        j = matrix.length - 1 - j;
+                        break;
+                    case 1:
+                    case 3:
+                        i = matrix.length - 1 - i;
+                        //j = matrix.length - 1 - j;
+                        break;
+                    default:
+                        break;
+                }
+
+                if (!arr[i][j]) {
+                    // 记录被覆盖的值
+                    tempValueNew = matrix[i][j];
+                    arr[i][j] = true;
+
+                    // 填充值
+                    matrix[i][j] = tempValueOld;
+                }
+            }
+        }
+    }
+
+    public static void rotate2(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = 0; j < (n + 1) / 2; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = temp;
+            }
+        }
+    }
+}
