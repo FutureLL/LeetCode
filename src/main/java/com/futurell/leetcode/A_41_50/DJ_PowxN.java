ecf1b8cb07a9a488ece0bc0dcfdd894a353ebc74
lilei58 <13186102535@163.com>
2021-09-08 08:50:26 +0800

    DJ_PowxN

---
+++ src/main/java/com/futurell/leetcode/A_41_50/DJ_PowxN.java
@@ -0,0 +1,34 @@
+package com.futurell.leetcode.A_41_50;
+
+/**
+ * @Description: 50. Pow(x, n)
+ * @Author: lilei58
+ * @Date: Created in 2021/9/8 上午8:46
+ * 实现 pow(x, n)，即计算 x 的 n 次幂函数。
+ *
+ * 示例 1：
+ *  输入：x = 2.00000, n = 10
+ *  输出：1024.00000
+ *
+ * 示例 2：
+ *  输入：x = 2.10000, n = 3
+ *  输出：9.26100
+ *
+ * 示例 3：
+ *  输入：x = 2.00000, n = -2
+ *  输出：0.25000
+ *  解释：2-2 = 1/22 = 1/4 = 0.25
+ */
+public class DJ_PowxN {
+
+    public static void main(String[] args) {
+        double v = myPow(2.10000, 3);
+        int i = 0;
+    }
+
+    public static double myPow(double x, int n) {
+
+        return 0.0;
+
+    }
+}
